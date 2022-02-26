0019aff8c2f393e94864c22ddf8730b84af1a443
Bhone Pyae Kyaw <bhonepyaekyaw1899@gmail.com>
2022-02-26 11:47:30 +0630

    final work

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(180, 29%, 50%)",
+        lightGrayishCyanBg: "hsl(180, 52%, 96%)",
+        lightGrayishCyanFt: "hsl(180, 31%, 95%)",
+        darkGrayishCyan: "hsl(180, 8%, 52%)",
+        veryDarkGrayishCyan: "hsl(180, 14%, 20%)",
+      },
+    },
+  },
+  plugins: [],
+};
